9259c90087d6ce311704b0e3c70163b9a0105234
Matheus Farias <mfarias@mmcafe.com.br>
2020-09-29 18:03:56 -0300

    format dates for json view

--- api/models/term.go
+++ api/models/term.go
@@ -9,16 +9,25 @@ import (
 
 // Term - table name: terms
 type Term struct {
-	ID           uuid.UUID     `gorm:"type:uuid;primary_key;" json:"id"`
-	Name         string        `gorm:"size:255;not null;unique" json:"name"`
-	Pronuciation string        `gorm:"size:255;" json:"pronuciation"`
-	Definition   string        `gorm:"type:text;" json:"definition"`
-	Synonyms     string        `gorm:"type:text;" json:"synonyms"`
-	RelatedTerms []RelatedTerm `json:"related_terms"`
-	CreatedByID  uuid.UUID     `gorm:"type:uuid;not null" json:"created_by_id"`
-	CreatedAt    time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedByID  uuid.UUID     `gorm:"type:uuid;not null" json:"updated_by_id"`
-	UpdatedAt    time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	ID                uuid.UUID     `gorm:"type:uuid;primary_key;" json:"id"`
+	Name              string        `gorm:"size:255;not null;unique" json:"name"`
+	Pronuciation      string        `gorm:"size:255;" json:"pronuciation"`
+	Definition        string        `gorm:"type:text;" json:"definition"`
+	Synonyms          string        `gorm:"type:text;" json:"synonyms"`
+	RelatedTerms      []RelatedTerm `json:"related_terms"`
+	CreatedByID       uuid.UUID     `gorm:"type:uuid;not null" json:"created_by_id"`
+	CreatedAt         time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
+	CreatedAtFormated string        `sql:"-" json:"created_at"`
+	UpdatedByID       uuid.UUID     `gorm:"type:uuid;not null" json:"updated_by_id"`
+	UpdatedAt         time.Time     `gorm:"default:CURRENT_TIMESTAMP" json:"-"`
+	UpdatedAtFormated string        `sql:"-" json:"updated_at"`
+}
+
+// AfterFind - format dates
+func (term *Term) AfterFind(tx *gorm.DB) (err error) {
+	term.UpdatedAtFormated = term.UpdatedAt.Format("02/01/2006 15:04:05")
+	term.CreatedAtFormated = term.CreatedAt.Format("02/01/2006 15:04:05")
+	return
 }
 
 // BeforeCreate - create new uuid
